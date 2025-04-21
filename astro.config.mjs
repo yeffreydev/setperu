e5c00c8b77d5e244e017c359cae0cc71db087de7
Yeffrey Espinoza <yeffreyespinoza200@gmail.com>
2025-04-21 01:01:31 -0500

    updated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,19 @@ import { defineConfig } from "astro/config";
 
 import robotsTxt from "astro-robots-txt";
 
+const _dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 // https://astro.build/config
 export default defineConfig({
   site: import.meta.env.DEV
     ? "http://localhost:4321"
     : "https://luna-landing-rust.vercel.app/",
   integrations: [tailwind(), sitemap(), robotsTxt()],
+  resolve: {
+    alias: {
+      "@assets": `${_dirname}/src/assets`,
+      "@components": `${_dirname}/src/components`,
+      "@content": `${_dirname}/src/content`,
+    },
+  },
 });
