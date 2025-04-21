e4afa377fd5514b80e3a466aacbdb7ff50d6c0ea
Yeffrey Espinoza <yeffreyespinoza200@gmail.com>
2025-04-21 01:35:49 -0500

    updated xd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
 import path from "path";
 import url from "url";
@@ -10,15 +11,9 @@ const _dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
   site: import.meta.env.DEV
     ? "http://localhost:4321"
     : "https://luna-landing-rust.vercel.app/",
   integrations: [tailwind(), sitemap(), robotsTxt()],
-  resolve: {
-    alias: {
-      "@assets": `${_dirname}/src/assets`,
-      "@components": `${_dirname}/src/components`,
-      "@content": `${_dirname}/src/content`,
-    },
-  },
 });
